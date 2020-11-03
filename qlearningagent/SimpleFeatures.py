3bac68b9d0e796c5c462791b03e395c626722dc9
Mathias Maes <mathiasmaesdev@gmail.com>
2020-11-03 18:37:16 +0100

    Fixed new state

--- qlearningagent/SimpleFeatures.py
+++ qlearningagent/SimpleFeatures.py
@@ -1,23 +1,25 @@
-from .Features import Features, Feature
+from Features import Features, Feature
+from State import State
+
 import chess
 
 class SimpleFeatures(Features):
     def __init__(self):
         Features.__init__()
-        self.append(TestFeature())
+        self.append(AttackersFeature())
 
 
 class AttackersFeature(Feature):
-    def calculateValue(self, nextState: chess.Board, player):
+    def calculateValue(self, nextState: State):
 
         opponentSquareSet = chess.SquareSet()
         for piece_type in range(1, 7):
-            squares = nextState.pieces(piece_type, not player)
+            squares = nextState.getBoard().pieces(piece_type, not nextState.isWhite())
             for square in squares:
                 opponentSquareSet.add(square)
 
         sum = 0
         for square in opponentSquareSet:
-            sum += len(nextState.attackers(player, square))
+            sum += len(nextState.getBoard().attackers(nextState.isWhite(), square))
 
         return sum
