163136e0514755c4e2048d9b72690d6f39a7c493
Tijs Van Alphen <tijs.van.alphen@hotmail.com>
2020-11-08 13:34:13 +0100

    PositionParser

---
+++ chessUtil/PositionParser.py
@@ -0,0 +1,7 @@
+import chess
+
+def getRowColumn(square: chess.Square):
+    return (chess.square_rank(square), chess.square_file(square))
+
+def getSquareFromRowColumn(row, column):
+    return chess.Square(row*8 + column)
