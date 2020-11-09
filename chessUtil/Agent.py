5c96521ea2a7bb6d066c28b5816cb87b6e268d7b
Mathias Maes <mathiasmaesdev@gmail.com>
2020-11-09 18:26:13 +0100

    Generalised agent

---
+++ chessUtil/Agent.py
@@ -0,0 +1,17 @@
+from chessUtil.State import State
+
+class Agent:
+    def __init__(self, goTime):
+        self.goTime = goTime
+
+    def setGoTime(self, goTime):
+        self.goTime = goTime
+
+    def update(self, state: State, nextState: State):
+        pass
+
+    def makeMove(self, state: State):
+        raise NotImplementedError()
+
+    def save(self):
+        raise NotImplementedError()
