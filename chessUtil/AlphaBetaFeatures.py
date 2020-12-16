1382389c015c3c2bf0de30526a00e049b4269930
Mathias Maes <mathias.maes.mm@gmail.com>
2020-12-16 18:10:19 +0100

    Remove disfunctional hash table

--- chessUtil/AlphaBetaFeatures.py
+++ chessUtil/AlphaBetaFeatures.py
@@ -28,40 +28,20 @@ class AlphaBeta(Feature):
         self.agent = ABAgent()
         self.agent.features = features
         self.mutex = Lock()
-        self.stateMap = {}
-        self.currentIndex = 0
         self.lastAction = None
         self.lastState = None
 
     def calculateValue(self, state: State, action, nextState: State):
         if self.mutex.acquire(blocking=False) is not False:
-            if self.getAction(state) is None:
+            if self.lastState is not state:
                 self.agent.setGoTime(state.getAgent().getGoTime())
                 self.agent.setDeltaTime(state.getAgent().getDeltaTime())
                 self.agent.setMaxDepth(state.getAgent().maxDepth)
-                self.addToMap(state, self.agent.makeMove(state))
+                self.lastState = state
+                self.lastAction = self.agent.makeMove(state)
             self.mutex.release()
 
         while self.mutex.locked():
-            time.sleep(0.001)
+            time.sleep(0.01)
 
-        return self.getAction(state) == action
-
-    def getAction(self, state: State):
-        if str(state.getBoard()) in self.stateMap:
-            return self.stateMap[str(state.getBoard())]
-        else:
-            return None
-
-    def addToMap(self, state: State, action):
-
-        self.currentIndex = (self.currentIndex + 1) % 10000
-        self.remove()
-
-        self.stateMap[str(state.getBoard())] = (self.currentIndex, action)
-
-    def remove(self):
-        for k in self.stateMap:
-            if self.stateMap[k][0] == self.currentIndex:
-                self.stateMap.pop(k)
-                return
+        return self.lastAction == action
